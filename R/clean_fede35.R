26ddda0f5365d50161ebddd9b0812e819a67f54e
BADOL Coline <cbbiologie@gmail.com>
2022-03-15 16:38:06 +0100

    création de deux fonction pour lire un fichier xlsx de fédé35 et pour mettre en dataframe les données du fédé35

---
+++ R/clean_fede35.R
@@ -0,0 +1,67 @@
+#' Mise d'un df brut des pêches fédés35 au format passerelle
+#'
+#' @param df_brut Dataframe données SD.
+#' @param crs_init Numérique. Code EPSG du CRS initial. Par défaut c'est 2154 (Lambert 93).
+#' @param crs_fin Numérique. Code EPSG du CRS de sortie. Par défaut c'est 4326 (WGS84).
+#'
+#' @return Un dataframe au format souhaité avec les coordonnées reprojetées
+#' @export
+#'
+#' @importFrom dplyr bind_cols mutate select mutate_at vars
+#' @importFrom sf st_drop_geometry
+#' @importFrom tidyr pivot_longer
+#'
+#' @examples
+#' \dontrun{
+#' fede35_clean <- fede35_brut %>%
+#' clean_fede35()
+#' }
+
+clean_fede35 <- function(df_brut, crs_init = 2154, crs_fin = 4326) {
+
+  # coords <- get_coords(sf_obj = df_brut,
+  #                      crs_init = crs_init,
+  #                      crs_fin = crs_fin)
+  df <- df_brut %>%
+    # st_drop_geometry() %>%
+    # bind_cols(coords) %>%
+    # pivot_longer(cols = ABH:VAX,
+    #              names_to = "code_espece",
+    #              values_to = "effectif") %>%
+
+    mutate(
+      code_exutoire = NA,
+      code_station = NA,
+      date_peche = Date,
+      annee = str_sub(date_peche, 1, 4),
+      annee = as.integer(annee),
+      source_donnee = "Fede 35",
+      code_espece = Espèce,
+      effectif = str_to_upper(`Nb Individus`),
+      type_peche = Type,
+      localisation = `Cours d'eau`,
+      x_wgs84 = X,
+      y_wgs84 = Y) %>%
+
+    select(
+      code_exutoire,
+      code_station,
+      localisation,
+      x_wgs84,
+      y_wgs84,
+      date_peche,
+      annee,
+      source_donnee,
+      type_peche,
+      code_espece,
+      effectif
+    ) %>%
+
+    mutate_at(vars(code_station, localisation, date_peche),
+              as.character)
+
+  df
+
+}
+
+clean_fede35(fede35)
--- R/lire_xlsx_fede35.R
+++ R/lire_xlsx_fede35.R
@@ -1,11 +1,10 @@
 #' Assemblage des fichiers annuels
 #'
-#' Cette fonction permet d'assembler - en les empilant - les fichiers csv issus de la fonction
-#'     telecharger_fichiers_onde_annuels() en un unique dataframe. La sortie est le dataframe national
-#'     pour l'ensemble des années où la donnée est disponible.
+#' Cette fonction permet d'assembler - en les empilant - les fichiers xlsx en homogénéisant
+#'     les noms des colonnes en un unique dataframe.
 #'
-#' @param fichier_reference Caractère. Chemin vers le fichier excel dont les noms de colonnes vont servir 
-#' de référence pour homogénéiser les noms des colonnes des différents fichiers excel
+#' @param fichier_reference Caractère. Chemin vers le fichier excel dont les noms de colonnes vont servir
+#'     de référence pour homogénéiser les noms des colonnes des différents fichiers excel
 #'
 #' @return Dataframe national pour l'ensemble des années de données disponibles
 #' @export
@@ -16,50 +15,50 @@
 #'
 #' @examples
 #' \dontrun{
-#' onde <- assembler_fichiers_onde_annuels_csv(
-#' annual_onde_files_dir = "raw_data/fichiers_onde_annuels_zippes"
+#' data <- lire_xlsx_fede35(repertoire="raw_data",
+#' fichier_reference = "CR op pêche elec FD35 2020-VF.xlsx")
 #' )
 #' }
 #'
 lire_xlsx_fede35 <- function(repertoire, fichier_reference = "CR op pêche elec FD35 2020-VF.xlsx") {
-  
+
   #Lecture d'un seul fichier excel qui range les variables dans l'ordre choisi
   lire_un_fichier <- function(fichier,ordre_var) {
     data <- readxl::read_xlsx(fichier)
-    
+
     #!str_detect pour mettre la négation
     if(!str_detect("Code station", names(data)))
     {
-      data <- data %>% 
+      data <- data %>%
         mutate(`Code station` = NA)
     }
-    
-    data <- data %>% 
+
+    data <- data %>%
       select(ordre_var)
-    
+
     return(data)
   }
-  
+
   # liste des fichiers .xlsx
   xlsx_files <- list.files(path = repertoire,
                           pattern = "CR op pêche elec FD35",
                           full.names = TRUE)
-  
-  variables <- readxl::read_xlsx(paste0(repertoire, "/", fichier_reference)) %>% 
-    names() %>% 
+
+  variables <- readxl::read_xlsx(paste0(repertoire, "/", fichier_reference)) %>%
+    names() %>%
   .[-1]
-  
+
   # ces fichiers sont lus puis "empilés"
   # utilisation de rbind qui est plus tout-terrain que map() %>% bind_rows() ou mp_df()
   FD35 <- purrr::map(.x = xlsx_files,
                      .f = lire_un_fichier,
                      ordre_var=variables) %>%
     reduce(rbind)
-  
+
   # # # renommage des variables pour enlever les caractères < et >
   # names(onde) <- names(onde) %>%
   #   str_replace_all(pattern = '[<>]', replacement = '')
-  # 
+  #
   # # # mise des variables au bon format
   # onde <- onde %>%
   #   mutate(Annee = as.integer(Annee),
@@ -67,9 +66,9 @@ lire_xlsx_fede35 <- function(repertoire, fichier_reference = "CR op pêche elec 
   #          CoordXSiteHydro = as.numeric(CoordXSiteHydro),
   #          CoordYSiteHydro = as.numeric(CoordYSiteHydro),
   #          ProjCoordSiteHydro = as.integer(ProjCoordSiteHydro))
-  # 
+  #
   return(FD35)
-  
+
 }
 
 data <- lire_xlsx_fede35(repertoire="raw_data")
