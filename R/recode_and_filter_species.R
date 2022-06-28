0c70f292d2bd1fda86c3b72ee6139ada04061235
PascalIrz <pascal.irz@gmail.com>
2022-06-28 08:10:17 +0200

    màj recode_and_filter_species() / carassins

--- R/recode_and_filter_species.R
+++ R/recode_and_filter_species.R
@@ -2,7 +2,8 @@
 #'
 #' Par exemple les carpes cuir, miroir, etc. sont regroupées sous un unique code CCX.
 #' Idem pour les vandoises en VAX.
-# 'Dans l'ouest Finistère, il n'y a que de l'épinoche => recodage de l'épinochette sur cette zone.
+#' Dans l'ouest Finistère, il n'y a que de l'épinoche => recodage de l'épinochette sur cette zone.
+#' Tous les carassins sont des argentés.
 #'
 #' @param df Dataframe standardisé contenant les données.
 #' @param sp_to_remove Vecteur texte contenant les codes à trois lettres des taxons à
@@ -32,10 +33,10 @@ recode_and_filter_species <- function(df, sp_to_remove = NA) {
     mutate(code_espece = str_replace(code_espece, pattern = "CCU", replacement = "CCO"),
            code_espece = str_replace(code_espece, pattern = "CCX", replacement = "CCO"),
            code_espece = str_replace(code_espece, pattern = "CMI", replacement = "CCO"),
-           code_espece = str_replace(code_espece, pattern = "CAG", replacement = "CAS"),
-           code_espece = str_replace(code_espece, pattern = "CAD", replacement = "CAS"),
-           code_espece = str_replace(code_espece, pattern = "CAA", replacement = "CAS"),
-           code_espece = str_replace(code_espece, pattern = "CAX", replacement = "CAS"),
+           code_espece = str_replace(code_espece, pattern = "CAS", replacement = "CAG"),
+           code_espece = str_replace(code_espece, pattern = "CAD", replacement = "CAG"),
+           code_espece = str_replace(code_espece, pattern = "CAA", replacement = "CAG"),
+           code_espece = str_replace(code_espece, pattern = "CAX", replacement = "CAG"),
            code_espece = str_replace(code_espece, pattern = "VAN", replacement = "VAR"),
            code_espece = ifelse(code_espece == "EPT" & x_wgs84 < (-4.1), "EPI", code_espece))
 
